471e2c530a253605be2f2a9e89ce80b2444ebc5e
AziatAbd <abdimalikovaziat@gmail.com>
2025-09-08 07:43:05 +0600

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import Login from "./pages/Login";
 
 function App() {
   const router = createHashRouter([
+    // {
+    //   path: "/",
+    //   element: <Login />,
+    // },
     {
       path: "/",
-      element: <Login />,
-    },
-    {
-      path: "/main",
       element: <MainPage />,
     },
   ]);
 
   return <RouterProvider router={router} />;
 }
- 
+
 export default App;
